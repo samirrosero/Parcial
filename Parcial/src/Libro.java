f29e0bd4d4bf244b60f0ae0fb60f94876f641df1
samir <samir_armero16@hotmail.com>
2025-09-02 19:34:22 -0500

    subiendo parte de Clase Libro

---
+++ Parcial/src/Libro.java
@@ -0,0 +1,54 @@
+public class Libro {
+    public String titulo;
+    public String autor;
+    public int numeroEjemplares;
+    public int numeroEjemplaresPrestados;
+
+    // Constructor
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String autor, int numeroEjemplares, int numeroEjemplaresPrestados) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroEjemplares = numeroEjemplares;
+        this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroEjemplares() {
+        return this.numeroEjemplares;
+    }
+
+    public void setNumeroEjemplares(int numeroEjemplares) {
+        this.numeroEjemplares = numeroEjemplares;
+    }
+
+    public int getNumeroEjemplaresPrestados() {
+        return this.numeroEjemplaresPrestados;
+    }
+
+    public void setNumeroEjemplaresPrestados(int numeroEjemplaresPrestados) {
+        this.numeroEjemplaresPrestados = numeroEjemplaresPrestados;
+    }
+
+    
+
+    
+}
