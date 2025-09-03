90d151391f8a628a2321782a59d442e7aac5e900
Valeri-Solis1 <vasolis@estudiante.uniajc.edu.co>
2025-09-02 20:54:50 -0500

    libroo1

--- Parcial/src/App.java
+++ Parcial/src/App.java
@@ -1,6 +1,6 @@
 public class App extends Libro {
     public static void main(String[] args) throws Exception {
-        Libro libro1 = new Libro("Todo lo que me falta por contar","valeri solis", 7,10 );
+        Libro libro1 = new Libro("Todo lo que me falta por contar","andres solis", 7,10 );
         
     }
 }
